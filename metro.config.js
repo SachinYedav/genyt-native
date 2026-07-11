ac047bd83bb82c24b95958a85352fa06c42ae7ed
Sachin Yadav <frontenddeveloper1913@gmail.com>
2026-07-11 11:36:31 +0530

    fix: prevent dual nitro module installation crash in production

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Prevent Metro from resolving duplicate packages from the local module's node_modules
+const path = require('path');
+config.resolver.blockList = [
+  ...config.resolver.blockList || [],
+  new RegExp(path.resolve(__dirname, 'modules/genyt-youtube-extractor/node_modules') + '/.*'),
+];
+
 module.exports = config;
